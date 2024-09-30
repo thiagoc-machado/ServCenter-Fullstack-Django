b36ebef92f95993d3dae0ed69c2bb573d85b5060
thiagocmachado@yahoo.com.br <thiagocmachado@yahoo.com.br>
2024-09-30 13:23:11 +0200

    logo error

--- config/urls.py
+++ config/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('new/', views.new_config, name='new_config'),
     path('', views.config, name="config"),
     path('edit/', views.edit_config, name="edit_config"),
-    path('categoria_in/', views.categoria_in, name="categoria_in"),
-    path('categoria_out/', views.categoria_out, name="categoria_out"),
+    path('categoria_in/', views.categoria_in, name="categoria_in"),  # type: ignore
+    path('categoria_out/', views.categoria_out, name="categoria_out"),  # type: ignore
     path('del_categoria_in/<int:id>', views.del_categoria_in, name="del_categoria_in"),
     path('del_categoria_out/<int:id>', views.del_categoria_out, name="del_categoria_out"),
 ] 
